c7081b35672b12869c5cbe76a213f7d1c132c952
zetazx coder7 <zetazxcoder@outlook.com>
2023-10-20 18:38:47 +0000

    sla

--- bot.py
+++ bot.py
@@ -1,5 +1,17 @@
 import requests
 import json
+import datetime
+
+def hora(data):
+    dt = datetime.datetime.fromisoformat(data)
+    hora = dt.hour
+    minuto = dt.minute
+    hora_formatada = f"{hora:02d}"
+    minuto_formatado = f"{minuto:02d}"
+
+    # Exibir o resultado
+    return f"{hora_formatada}:{minuto_formatado}"
+
 
 def transmissao(id):
     try:
@@ -22,18 +34,22 @@ def transmissao(id):
     except:
         return False
     
-    
-response = requests.get("http://mobileapi.365scores.com/Data/Games/?apptype=1&appversion=5.8.22&favoritecompetitions=7,11,17,25,35,102,113,115,116,389,572,613&lang=31&light=true&oddsformat=1&onlymajorgames=false&onlyontv=true&startdate=20%2F10%2F2023&enddate=20%2F10%2F2023&storeversion=5.8.22&theme=dark&tz=93&uc=21&sport=1")
-response = json.loads(response.text)
-games = response['Games']
+def jogos():
+    response = requests.get("https://webws.365scores.com/web/games/myscores/?appTypeId=5&langId=31&timezoneName=America/Fortaleza&userCountryId=323&competitions=6071,5096,113,572,115,102,11,7,116,114,389,573,13,17,595,35,25,332,73,104,649,5501,6164,76,10,15,6316,23,37&competitors=1267&startDate=20/10/2023&endDate=20/10/2023&showOdds=true&withSuggested=true")
+    response = json.loads(response.text)
+    games = response['games']
 
-for game in games:
-    if game['SID'] == 1:
-        id = game['ID']
-        trans = transmissao(id)
-        
-        if trans != False:
-            timeCasa = game['Comps'][0]['Name']
-            timeFora = game['Comps'][1]['Name']
-            print(f"{timeCasa} x {timeFora} - {trans['transmissao']}")
+    for game in games:
+        if game['sportId'] == 1:
+            id = game['id']
+            trans = transmissao(id)
+            
+            if trans != False:
+                diajogo = game['startTime']
+                diajogo = hora(diajogo)
+                timeCasa = game['homeCompetitor']['name']
+                timeFora = game['awayCompetitor']['name']
+                comp = game['competitionDisplayName']
+                print(f"{diajogo} - {timeCasa} x {timeFora} - {comp} - {trans['transmissao']}")
 
+jogos()
