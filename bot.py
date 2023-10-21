0b71aa849a8748c3ec8929a24b5a0ff173ef3cd6
zetazxcoder7 <147059438+zetazxcoder7@users.noreply.github.com>
2023-10-21 03:06:54 +0000

    now

--- bot.py
+++ bot.py
@@ -2,6 +2,27 @@ import requests
 import json
 import datetime
 import re
+from collections import defaultdict
+import time
+import telebot
+import schedule
+
+dono = [6495221880, 5178639176]
+
+def hoje():
+    # Obtém a data atual
+    data_atual = datetime.datetime.now()
+
+    # Formata a data
+    data_formatada = data_atual.strftime('%d/%m/%Y')
+
+    return data_formatada
+
+bot = telebot.TeleBot("6526858455:AAEpwIzrra2bjm5V10IWmrv_rO1VybkDaNc")
+
+def send_msg(msg):
+    for id in dono:
+        bot.send_message(id, msg, parse_mode="Markdown")
 
 def hora(data):
     dt = datetime.datetime.fromisoformat(data)
@@ -12,7 +33,6 @@ def hora(data):
 
     # Exibir o resultado
     return f"{hora_formatada}:{minuto_formatado}"
-
 
 def transmissao(id):
     try:
@@ -36,7 +56,7 @@ def transmissao(id):
         return False
     
 def jogos():
-    response = requests.get("https://webws.365scores.com/web/games/myscores/?appTypeId=5&langId=31&timezoneName=America/Fortaleza&userCountryId=323&competitions=6071,5096,113,572,115,102,11,7,116,114,389,573,13,17,595,35,25,332,73,104,649,5501,6164,76,10,15,6316,23,37&competitors=1267&startDate=20/10/2023&endDate=20/10/2023&showOdds=true&withSuggested=true")
+    response = requests.get(f"https://webws.365scores.com/web/games/myscores/?appTypeId=5&langId=31&timezoneName=America/Fortaleza&userCountryId=323&competitions=6071,5096,113,572,115,102,11,7,116,114,389,573,13,17,595,35,25,332,73,104,649,5501,6164,76,10,15,6316,23,37&competitors=1267&startDate={hoje()}&endDate={hoje()}&showOdds=true&withSuggested=true")
     response = json.loads(response.text)
     games = response['games']
 
@@ -69,5 +89,28 @@ def jogos():
     return games
         
 
+def formatar_jogos():
+    dados = jogos()
+    jogos_hora = defaultdict(list)
 
+    for jogo in dados:
+        jogos_hora[jogo['data']].append(jogo)
+
+    resposta = "Jogos de Hoje!! \n\n"
+    for hora in jogos_hora:
+        partida = jogos_hora[hora]
+        resposta += f"{hora} -\n\n"
+        for jogo in partida:
+            resposta += f"{jogo['slogan']}\n{jogo['camp']}\n{jogo['transmissao']['transmissao']}\n\n"
+
+    send_msg(resposta)
+    print(resposta)
+
+
+schedule.every().day.at("03:06").do(formatar_jogos)
+
+while True:
+    schedule.run_pending()
+    print('Estou Esperando...')
+    time.sleep(1)
 
